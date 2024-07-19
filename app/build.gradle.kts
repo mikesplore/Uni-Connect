8d3d7fe0e2d23d4869409e0ec50c9b1295198fe5
mikesplore <mikepremium8@gmail.com>
2024-07-19 13:12:22 +0300

    Adding youtube player

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 
 dependencies {
     //these are my added dependencies
+    implementation (libs.core)
     implementation(libs.coil.compose)
     implementation(libs.androidx.core.splashscreen)
     implementation (libs.google.accompanist.pager)
