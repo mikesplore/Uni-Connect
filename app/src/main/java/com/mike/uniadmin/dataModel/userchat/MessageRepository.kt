d0535100e9a3247d49495695205ce633dd6a69ed
mikesplore <mikepremium8@gmail.com>
2024-07-24 14:36:54 +0300

    Combined the chats and groups

--- app/src/main/java/com/mike/uniadmin/dataModel/userchat/MessageRepository.kt
+++ app/src/main/java/com/mike/uniadmin/dataModel/userchat/MessageRepository.kt
@@ -4,40 +4,61 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.mike.uniadmin.dataModel.groupchat.ChatEntity
 import com.mike.uniadmin.model.MyDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MessageRepository {
+val viewModelScope = CoroutineScope(Dispatchers.Main)
+
+class MessageRepository(private val messageDao: MessageDao) {
     private val database = FirebaseDatabase.getInstance().reference
 
-    fun fetchMessages(path: String, onResult: (List<Message>) -> Unit) {
-        database.child(path).addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val messages = mutableListOf<Message>()
-                for (childSnapshot in snapshot.children) {
-                    val message = childSnapshot.getValue(Message::class.java)
-                    message?.let { messages.add(it) }
-                }
-                onResult(messages)
-            }
+    fun fetchMessages(path: String, onResult: (List<MessageEntity>) -> Unit) {
+        viewModelScope.launch {
+            val cachedChats = messageDao.getMessages(path)
+            if (cachedChats.isNotEmpty()) {
+                onResult(cachedChats)
+            } else {
+                    database.child(path).addValueEventListener(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            val messages = mutableListOf<MessageEntity>()
+                            for (childSnapshot in snapshot.children) {
+                                val message = childSnapshot.getValue(MessageEntity::class.java)
+                                message?.let { messages.add(it) }
+                            }
+                            com.mike.uniadmin.dataModel.groupchat.viewModelScope.launch {
+                                messageDao.insertMessages(messages)
+                                onResult(messages)
+                            }
+                        }
 
-            override fun onCancelled(error: DatabaseError) {
-                // Handle the read error (e.g., log the error)
-                println("Error reading messages: ${error.message}")
+                        override fun onCancelled(error: DatabaseError) {
+                            // Handle the read error (e.g., log the error)
+                            println("Error reading messages: ${error.message}")
+                        }
+                    })
             }
-        })
+        }
     }
 
-    fun saveMessage(message: Message, path: String, onComplete: (Boolean) -> Unit) {
-        MyDatabase.database.child(path).push().setValue(message)
-            .addOnCompleteListener {
-                onComplete(true)
-            }
-            .addOnFailureListener {
-                onComplete(false)
-            }
+    fun saveMessage(message: MessageEntity, path: String, onComplete: (Boolean) -> Unit) {
+        viewModelScope.launch {
+            messageDao.insertMessages(listOf(message))
+            database.child(path).push().setValue(message)
+                .addOnCompleteListener {
+                    onComplete(true)
+                }
+                .addOnFailureListener {
+                    onComplete(false)
+                }
+        }
     }
 
     fun deleteMessage(messageId: String, onSuccess: () -> Unit, onFailure: (Exception?) -> Unit) {
+        viewModelScope.launch {
+        messageDao.deleteMessage(messageId)
         database.child(messageId).removeValue() // Use the consistent database reference
             .addOnSuccessListener {
                 onSuccess()
@@ -45,5 +66,5 @@ class MessageRepository {
             .addOnFailureListener { exception ->
                 onFailure(exception)
             }
-    }
+    }}
 }
