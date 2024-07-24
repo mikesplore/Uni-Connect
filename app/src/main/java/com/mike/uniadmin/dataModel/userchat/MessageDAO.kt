a55151c7c974335428004f6b7ffb2b593ab56814
mikesplore <mikepremium8@gmail.com>
2024-07-24 14:36:51 +0300

    Combined the chats and groups

---
+++ app/src/main/java/com/mike/uniadmin/dataModel/userchat/MessageDAO.kt
@@ -0,0 +1,18 @@
+package com.mike.uniadmin.dataModel.userchat
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface MessageDao {
+    @Query("SELECT * FROM messages WHERE id LIKE '%' || :path || '%'")
+    suspend fun getMessages(path: String): List<MessageEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMessages(messages: List<MessageEntity>)
+
+    @Query("DELETE FROM messages WHERE id = :messageId")
+    suspend fun deleteMessage(messageId: String)
+}
