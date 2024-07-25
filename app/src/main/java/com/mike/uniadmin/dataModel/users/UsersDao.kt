3d7448a62128cbf2e33646824db5c9a19c89485c
mikesplore <mikepremium8@gmail.com>
2024-07-25 17:35:01 +0300

    user database sorted. next up is the courses

---
+++ app/src/main/java/com/mike/uniadmin/dataModel/users/UsersDao.kt
@@ -0,0 +1,67 @@
+package com.mike.uniadmin.dataModel.users
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.mike.uniadmin.dataModel.groupchat.ChatEntity
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM users")
+    suspend fun getUsers(): List<UserEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(users: UserEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUsers(users: List<UserEntity>) // New method to insert multiple users
+
+    @Query("DELETE FROM users WHERE id = :userID")
+    suspend fun deleteUser(userID: String)
+
+    @Query("SELECT * FROM users WHERE email = :userEmail")
+    suspend fun getUserByEmail(userEmail: String): UserEntity?
+
+    @Query("SELECT * FROM users WHERE id = :userID")
+    suspend fun getUserByID(userID: String): UserEntity?
+}
+
+@Dao
+interface UserStateDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUserState(userState: UserStateEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUserStates(userStates: List<UserStateEntity>) // New method to insert multiple user states
+
+    @Query("SELECT * FROM userState WHERE :userID")
+    suspend fun getUserState(userID: String): UserStateEntity?
+
+    @Query("SELECT * FROM userState")
+    suspend fun getAllUserStates(): List<UserStateEntity>
+}
+
+
+
+@Dao
+interface AccountDeletionDao{
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAccountDeletion(accountDeletion: AccountDeletionEntity)
+
+    @Query ("SELECT * FROM accountDeletion WHERE id = :userID")
+    suspend fun getAccountDeletion(userID: String): AccountDeletionEntity?
+
+}
+
+
+
+@Dao
+interface UserPreferencesDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUserPreferences(userPreferences: UserPreferencesEntity)
+
+    @Query ("SELECT * FROM userPreferences WHERE id = :userID")
+    suspend fun getUserPreferences(userID: String): UserPreferencesEntity?
+
+}
