84bc7d04c03d489d0ef8f58750b65c064a8bf60d
mikesplore <mikepremium8@gmail.com>
2024-07-26 15:43:51 +0300

    Some Bug fixes

--- app/src/main/java/com/mike/uniadmin/dataModel/announcements/AnnouncementRepository.kt
+++ app/src/main/java/com/mike/uniadmin/dataModel/announcements/AnnouncementRepository.kt
@@ -1,45 +1,96 @@
 package com.mike.uniadmin.dataModel.announcements
 
-
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.mike.uniadmin.dataModel.users.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class AnnouncementRepository {
+val announcementViewModelScope = CoroutineScope(Dispatchers.Main)
+
+class AnnouncementRepository(private val announcementsDao: AnnouncementsDao) {
     private val database = FirebaseDatabase.getInstance().reference.child("Announcements")
 
-    fun fetchAnnouncements(onResult: (List<Announcement>) -> Unit) {
+    init {
+        startAnnouncementsListener()
+    }
+
+    private fun startAnnouncementsListener() {
         database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                val announcements = mutableListOf<Announcement>()
+                val announcements = mutableListOf<AnnouncementEntity>()
                 for (childSnapshot in snapshot.children) {
-                    val announcement = childSnapshot.getValue(Announcement::class.java)
+                    val announcement = childSnapshot.getValue(AnnouncementEntity::class.java)
                     announcement?.let { announcements.add(it) }
                 }
-                onResult(announcements)
+
+                viewModelScope.launch {
+                    announcementsDao.insertAnnouncements(announcements)
+                }
             }
 
             override fun onCancelled(error: DatabaseError) {
                 // Handle the read error (e.g., log the error)
-                println("Error reading announcements: ${error.message}")
+                println("Error reading users: ${error.message}")
             }
         })
     }
 
-    fun saveAnnouncement(announcement: Announcement, onComplete: (Boolean) -> Unit) {
-        database.child(announcement.id).setValue(announcement).addOnCompleteListener { task ->
-            onComplete(task.isSuccessful)
+    fun fetchAnnouncements(onResult: (List<AnnouncementEntity>) -> Unit) {
+        viewModelScope.launch {
+            val cachedData = announcementsDao.getAnnouncements()
+            if (cachedData.isNotEmpty()) {
+                onResult(cachedData)
+            } else {
+                // Fetch from remote database if local database is empty
+                database.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val announcements = mutableListOf<AnnouncementEntity>()
+                        for (childSnapshot in snapshot.children) {
+                            val announcement =
+                                childSnapshot.getValue(AnnouncementEntity::class.java)
+                            announcement?.let { announcements.add(it) }
+                        }
+
+                        viewModelScope.launch {
+                            announcementsDao.insertAnnouncements(announcements)
+                            onResult(announcements)
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        // Handle the read error (e.g., log the error)
+                        println("Error reading users: ${error.message}")
+                    }
+                })
+            }
         }
     }
 
-    fun deleteAnnouncement(announcementId: String, onSuccess: () -> Unit, onFailure: (Exception?) -> Unit) {
-        database.child(announcementId).removeValue() // Use the consistent database reference
-            .addOnSuccessListener {
-                onSuccess()
+    fun saveAnnouncement(announcement: AnnouncementEntity, onComplete: (Boolean) -> Unit) {
+        announcementViewModelScope.launch {
+            announcementsDao.insertAnnouncements(listOf(announcement))
+            database.child(announcement.id).setValue(announcement).addOnCompleteListener { task ->
+                onComplete(task.isSuccessful)
             }
-            .addOnFailureListener { exception ->
-                onFailure(exception)
-            }
+        }
+    }
+
+    fun deleteAnnouncement(
+        announcementId: String,
+        onSuccess: () -> Unit,
+        onFailure: (Exception?) -> Unit
+    ) {
+        announcementViewModelScope.launch {
+            announcementsDao.deleteAnnouncement(announcementId)
+            database.child(announcementId).removeValue().addOnSuccessListener {
+                    onSuccess()
+                }.addOnFailureListener { exception ->
+                    onFailure(exception)
+                }
+        }
     }
 }
