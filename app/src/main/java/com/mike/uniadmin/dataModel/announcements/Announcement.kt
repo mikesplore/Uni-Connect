dbb4b598aad43a7e71fb2c150ee695fc8160471a
mikesplore <mikepremium8@gmail.com>
2024-07-26 15:43:49 +0300

    Some Bug fixes

--- app/src/main/java/com/mike/uniadmin/dataModel/announcements/Announcement.kt
+++ app/src/main/java/com/mike/uniadmin/dataModel/announcements/Announcement.kt
@@ -1,9 +1,25 @@
 package com.mike.uniadmin.dataModel.announcements
 
-data class Announcement(
-    val id: String = "",
-    val date: String = "",
-    val title: String = "",
-    val description: String = "",
-    val author: String = ""
-)
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "announcements")
+data class AnnouncementEntity(
+    @PrimaryKey val id: String,
+    val date: String? = null,
+    val title: String? = null,
+    val description: String? = null,
+    val authorID: String? = null,
+    val authorName: String? = null,
+    val imageLink: String? = null
+){
+    constructor(): this(
+        "",
+        null,
+        null,
+        null,
+        null,
+        null
+    )
+}
+
