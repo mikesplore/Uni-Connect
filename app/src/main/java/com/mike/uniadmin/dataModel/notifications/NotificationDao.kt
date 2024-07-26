9f198c6ee9ae1b87961c19d9f5ceebddf87cbe72
mikesplore <mikepremium8@gmail.com>
2024-07-26 15:44:29 +0300

    Some Bug fixes

---
+++ app/src/main/java/com/mike/uniadmin/dataModel/notifications/NotificationDao.kt
@@ -0,0 +1,18 @@
+package com.mike.uniadmin.dataModel.notifications
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface NotificationDao {
+    @Query("SELECT * FROM notifications")
+    suspend fun getAllNotifications(): List<NotificationEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNotification(notification: NotificationEntity) // For single insertion
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNotifications(notifications: List<NotificationEntity>) // For batch insertion
+}
