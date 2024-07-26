581e526216ff483322e27994c630104797421da8
mikesplore <mikepremium8@gmail.com>
2024-07-26 15:44:21 +0300

    Some Bug fixes

--- app/src/main/java/com/mike/uniadmin/Notes.kt
+++ app/src/main/java/com/mike/uniadmin/Notes.kt
@@ -1,3 +1,29 @@
 package com.mike.uniadmin
 
+//export database using this command
 //adb shell "run-as com.mike.uniadmin cp databases/UniAdminDatabase /storage/emulated/0/Download/ 2>&1; echo \$?" && adb pull /storage/emulated/0/Download/UniAdminDatabase
+
+
+
+//ISSUES TO CONSIDER
+//Add a section in the account deletion status to tell if a user has account deletion request ✅
+//work on the dark theme settings, the state should be correct✅
+//add alerts part that will show notifications
+//work on the github profile icon✅
+//download new font styles
+//work on the remaining bottom navigation items (timetable, assignments and the attendance)
+//the attendance here will be used to check the attendance of each student
+//implementing the Biometrics✅
+
+//SCREENS TO ADD
+//A screen for managing accounts such as deletion, adding, editing
+//A screen for managing courses(ADDING, EDITING AND DELETING)
+//A screen for statistics (IMPORTED FROM UNI KONNECT)
+//A screen for sending alert messages to users like upgrade needed, managing the app version
+
+
+
+//ADDITIONAL OPTIONAL FEATURES
+//feature for enabling offline mode
+//feature for liking announcements
+//feature for showing previous feedback of a current signed in user
