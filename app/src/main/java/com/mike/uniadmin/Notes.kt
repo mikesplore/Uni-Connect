541991e3f4ab56d16f22f17a1b4e74de9a153471
mikesplore <mikepremium8@gmail.com>
2024-07-27 13:29:10 +0300

    Updates

--- app/src/main/java/com/mike/uniadmin/Notes.kt
+++ app/src/main/java/com/mike/uniadmin/Notes.kt
@@ -8,15 +8,15 @@ package com.mike.uniadmin
 //ISSUES TO CONSIDER
 //Add a section in the account deletion status to tell if a user has account deletion request ✅
 //work on the dark theme settings, the state should be correct✅
-//add alerts part that will show notifications
+//add alerts part that will show notifications✅
 //work on the github profile icon✅
-//download new font styles
-//work on the remaining bottom navigation items (timetable, assignments and the attendance)
+//download new font styles✅
+//work on the remaining bottom navigation items (timetable, assignments and the attendance✅)
 //the attendance here will be used to check the attendance of each student
 //implementing the Biometrics✅
 
 //SCREENS TO ADD
-//A screen for managing accounts such as deletion, adding, editing
+//A screen for managing accounts such as deletion, adding, editing✅
 //A screen for managing courses(ADDING, EDITING AND DELETING)
 //A screen for statistics (IMPORTED FROM UNI KONNECT)
 //A screen for sending alert messages to users like upgrade needed, managing the app version
