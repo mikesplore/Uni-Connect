91c26a799e0866b03413cb9a16d7eb7cd41f9344
mikesplore <mikepremium8@gmail.com>
2024-07-27 13:31:02 +0300

    Updates

--- app/src/main/java/com/mike/uniadmin/dataModel/coursecontent/courseannouncements/CourseAnnouncements.kt
+++ app/src/main/java/com/mike/uniadmin/dataModel/coursecontent/courseannouncements/CourseAnnouncements.kt
@@ -3,20 +3,25 @@ package com.mike.uniadmin.dataModel.coursecontent.courseannouncements
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
 @Entity(tableName = "courseAnnouncements")
 data class CourseAnnouncement(
-  @PrimaryKey  val announcementID: String,
-    val date: String? = null,
-    val title: String? = null,
-    val description: String? = null,
-    val author: String? = null
-){
-  constructor(): this(
+  @PrimaryKey val announcementID: String,
+  val courseID: String? = null,
+  val title: String? = null,
+  val content: String? = null,
+  val date: String? = null,
+  val author: String? = null,
+  val description: String? = null
+)
+{
+  constructor(): this (
     "",
-    "",
-    "",
-    "",
-    ""
+    null,
+    null,
+    null,
+    null,
+    null,
+    null
   )
+
 }
