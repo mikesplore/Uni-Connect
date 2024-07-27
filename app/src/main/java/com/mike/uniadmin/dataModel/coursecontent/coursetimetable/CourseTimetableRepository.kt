2f8ea017f790c1c08695414ef3ad82fbe85ef274
mikesplore <mikepremium8@gmail.com>
2024-07-27 13:30:06 +0300

    Updates

--- app/src/main/java/com/mike/uniadmin/dataModel/coursecontent/coursetimetable/CourseTimetableRepository.kt
+++ app/src/main/java/com/mike/uniadmin/dataModel/coursecontent/coursetimetable/CourseTimetableRepository.kt
@@ -1,50 +1,69 @@
 package com.mike.uniadmin.dataModel.coursecontent.coursetimetable
 
+
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.mike.uniadmin.model.MyDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class CourseTimetableRepository {
-        private val database = FirebaseDatabase.getInstance().reference
+val viewModelScope = CoroutineScope(Dispatchers.Main)
 
-        //Course Content database
-        fun writeCourseTimetable(courseID: String, courseTimetable: CourseTimetable, onResult: (Boolean) -> Unit) {
-            val courseTimetableRef = MyDatabase.database.child("CourseContent")
-                .child(courseID)
-                .child("courseTimetable")
+class CourseTimetableRepository(private val courseTimetableDao: CourseTimetableDao) {
+    private val database = FirebaseDatabase.getInstance().reference.child("CourseContent")
 
-            courseTimetableRef.child(courseTimetable.timetableID).setValue(courseTimetable)
-                .addOnSuccessListener {
+    //Course Content database
+    fun writeCourseTimetable(
+        courseID: String,
+        courseTimetable: CourseTimetable,
+        onResult: (Boolean) -> Unit
+    ) {
+        com.mike.uniadmin.dataModel.coursecontent.coursedetails.viewModelScope.launch {
+            courseTimetableDao.insertCourseTimetable(courseTimetable)
+            val courseTimetableRef = MyDatabase.database.child(courseID)
+                .child("courseTimetables")
+
+            courseTimetableRef.child(courseTimetable.timetableID)
+                .setValue(courseTimetable).addOnSuccessListener {
                     onResult(true) // Indicate success
-                }
-                .addOnFailureListener { exception ->
+                }.addOnFailureListener { exception ->
                     println("Error writing timetable: ${exception.message}")
                     onResult(false) // Indicate failure
                 }
         }
-
-        fun getCourseTimetables(courseID: String, onResult: (List<CourseTimetable>) -> Unit) {
-            val courseTimetableRef = database.child("CourseContent")
-                .child(courseID)
-                .child("courseTimetable")
-
-            courseTimetableRef.addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    val timetables = mutableListOf<CourseTimetable>()
-                    for (childSnapshot in snapshot.children) {
-                        val timetable = childSnapshot.getValue(CourseTimetable::class.java)
-                        timetable?.let { timetables.add(it) }
-                    }
-                    onResult(timetables)
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    // Handle the read error (e.g., log the error)
-                    println("Error reading timetables: ${error.message}")
-                }
-            })
-        }
-
     }
+
+
+    fun getCourseTimetables(courseID: String, onResult: (List<CourseTimetable>) -> Unit) {
+        com.mike.uniadmin.dataModel.coursecontent.coursedetails.viewModelScope.launch {
+            val cachedData = courseTimetableDao.getCourseTimetables(courseID)
+            if (cachedData.isNotEmpty()) {
+                onResult(cachedData)
+            } else {
+                val courseTimetableRef =
+                    database.child(courseID).child("courseTimetables")
+
+                courseTimetableRef.addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val timetables = mutableListOf<CourseTimetable>()
+                        for (childSnapshot in snapshot.children) {
+                            val timetable =
+                                childSnapshot.getValue(CourseTimetable::class.java)
+                            timetable?.let { timetables.add(it) }
+                        }
+                        onResult(timetables)
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+
+                        println("Error reading timetables: ${error.message}")
+                    }
+                })
+            }
+        }
+    }
+
+}
