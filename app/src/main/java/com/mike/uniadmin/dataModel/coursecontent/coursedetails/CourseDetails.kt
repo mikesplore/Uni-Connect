4de2d8c41f8ab5d9ba629f75848c8f7f5121be98
mikesplore <mikepremium8@gmail.com>
2024-07-27 13:30:35 +0300

    Updates

--- app/src/main/java/com/mike/uniadmin/dataModel/coursecontent/coursedetails/CourseDetails.kt
+++ app/src/main/java/com/mike/uniadmin/dataModel/coursecontent/coursedetails/CourseDetails.kt
@@ -1,14 +1,32 @@
 package com.mike.uniadmin.dataModel.coursecontent.coursedetails
 
-data class CourseDetails(
-    val detailsID: String = "",
-    val courseName: String = "",
-    val courseCode: String = "",
-    val lecturer: String = "",
-    val numberOfVisits: String = "",
-    val courseDepartment: String = "",
-    val overview: String = "",
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "courseDetails")
+data class CourseDetail(
+    @PrimaryKey val detailID: String,
+    val courseName: String? = null,
+    val courseCode: String? = null,
+    val lecturer: String? = null,
+    val numberOfVisits: String? = null,
+    val courseDepartment: String? = null,
+    val overview: String? = null,
     val learningOutcomes: List<String> = emptyList(),
-    val schedule: String = "",
-    val requiredMaterials: String = ""
-)
+    val schedule: String? = null,
+    val requiredMaterials: String? = null
+){
+    constructor(): this(
+        "",
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        emptyList(),
+        null,
+        null
+    )
+
+}
