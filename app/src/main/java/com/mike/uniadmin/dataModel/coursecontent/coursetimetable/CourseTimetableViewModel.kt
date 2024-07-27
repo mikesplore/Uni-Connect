95388762835de93af8caf32dd68dbb8bcf9817e8
mikesplore <mikepremium8@gmail.com>
2024-07-27 13:30:02 +0300

    Updates

--- app/src/main/java/com/mike/uniadmin/dataModel/coursecontent/coursetimetable/CourseTimetableViewModel.kt
+++ app/src/main/java/com/mike/uniadmin/dataModel/coursecontent/coursetimetable/CourseTimetableViewModel.kt
@@ -1,6 +1,5 @@
 package com.mike.uniadmin.dataModel.coursecontent.coursetimetable
 
-
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
