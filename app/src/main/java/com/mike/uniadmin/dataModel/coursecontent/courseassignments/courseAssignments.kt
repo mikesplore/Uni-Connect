9968eef85b8009c34cbf92eeda4e551e730b3b1d
mikesplore <mikepremium8@gmail.com>
2024-07-27 13:30:52 +0300

    Updates

--- app/src/main/java/com/mike/uniadmin/dataModel/coursecontent/courseassignments/courseAssignments.kt
+++ app/src/main/java/com/mike/uniadmin/dataModel/coursecontent/courseassignments/courseAssignments.kt
@@ -1,9 +1,23 @@
 package com.mike.uniadmin.dataModel.coursecontent.courseassignments
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "courseAssignments")
 data class CourseAssignment(
-    val assignmentID: String = "",
-    val title: String = "",
-    val description: String = "",
-    val publishedDate: String = "",
-    val dueDate: String = ""
-)
+    @PrimaryKey  val assignmentID: String,
+    val courseCode: String? = null,
+    val dueDate: String? = null,
+    val title: String? = null,
+    val description: String? = null,
+    val publishedDate: String? = null
+){
+    constructor(): this(
+        "",
+        "",
+        "",
+        "",
+        ""
+    )
+}
