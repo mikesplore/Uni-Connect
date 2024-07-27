218e73f7d98fb74d61c31a4a1154bace8780f374
mikesplore <mikepremium8@gmail.com>
2024-07-27 13:30:13 +0300

    Updates

--- app/src/main/java/com/mike/uniadmin/dataModel/courses/CourseRepository.kt
+++ app/src/main/java/com/mike/uniadmin/dataModel/courses/CourseRepository.kt
@@ -10,8 +10,80 @@ import kotlinx.coroutines.launch
 
 val viewModelScope = CoroutineScope(Dispatchers.Main)
 
-class CourseRepository(private val courseDao: CourseDao) {
+class CourseRepository(private val courseDao: CourseDao, private val attendanceStateDao: AttendanceStateDao) {
     private val database = FirebaseDatabase.getInstance().reference.child("Courses")
+    private val attendanceStateDatabase = FirebaseDatabase.getInstance().reference.child("AttendanceStates")
+
+    init {
+        startCourseListener()
+        startAttendanceStateListener()
+    }
+
+    private fun startAttendanceStateListener() {
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val attendanceStates = mutableListOf<AttendanceState>()
+                for (childSnapshot in snapshot.children) {
+                    val attendanceState = childSnapshot.getValue(AttendanceState::class.java)
+                    attendanceState?.let { attendanceStates.add(it) }
+                }
+                viewModelScope.launch {
+                    attendanceStateDao.insertAttendanceStates(attendanceStates)
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle the read error (e.g., log the error)
+                println("Error reading courses: ${error.message}")
+            }
+        })
+    }
+
+    fun fetchAttendanceStates(onResult: (List<AttendanceState>) -> Unit) {
+        viewModelScope.launch {
+            attendanceStateDatabase.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val attendanceStates = mutableListOf<AttendanceState>()
+                    for (childSnapshot in snapshot.children) {
+                        val attendanceState = childSnapshot.getValue(AttendanceState::class.java)
+                        attendanceState?.let { attendanceStates.add(it) }
+                    }
+                    viewModelScope.launch {
+                        attendanceStateDao.insertAttendanceStates(attendanceStates)
+                    }
+                    onResult(attendanceStates)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle the read error (e.g., log the error)
+                    println("Error reading courses: ${error.message}")
+                    viewModelScope.launch {
+                        val cachedData = attendanceStateDao.getAttendanceStates()
+                        onResult(cachedData)
+                    }
+                }
+            })
+        }
+    }
+
+    fun saveAttendanceState(attendanceState: AttendanceState, onComplete: (Boolean) -> Unit) {
+        viewModelScope.launch {
+            attendanceStateDao.insertAttendanceState(attendanceState)
+            attendanceStateDatabase.child(attendanceState.courseID).setValue(attendanceState).addOnCompleteListener { task ->
+                onComplete(task.isSuccessful)
+            }
+        }
+    }
+
+    fun saveCourse(course: CourseEntity, onComplete: (Boolean) -> Unit) {
+        viewModelScope.launch {
+            courseDao.insertCourse(course)
+            database.child(course.courseCode).setValue(course).addOnCompleteListener { task ->
+                onComplete(task.isSuccessful)
+            }
+        }
+    }
 
 
     fun fetchCourses(onResult: (List<CourseEntity>) -> Unit) {
@@ -20,12 +92,15 @@ class CourseRepository(private val courseDao: CourseDao) {
             if (cachedData.isNotEmpty()) {
                 onResult(cachedData)
             } else {
-                database.addValueEventListener(object : ValueEventListener {
+                database.addListenerForSingleValueEvent(object : ValueEventListener {
                     override fun onDataChange(snapshot: DataSnapshot) {
                         val courses = mutableListOf<CourseEntity>()
                         for (childSnapshot in snapshot.children) {
                             val course = childSnapshot.getValue(CourseEntity::class.java)
                             course?.let { courses.add(it) }
+                        }
+                        viewModelScope.launch {
+                            courseDao.insertCourses(courses)
                         }
                         onResult(courses)
                     }
@@ -39,14 +114,6 @@ class CourseRepository(private val courseDao: CourseDao) {
         }
     }
 
-    fun saveCourse(course: CourseEntity, onComplete: (Boolean) -> Unit) {
-        viewModelScope.launch {
-            courseDao.insertCourse(listOf(course))
-            database.child(course.courseCode).setValue(course).addOnCompleteListener { task ->
-                onComplete(task.isSuccessful)
-            }
-        }
-    }
 
     fun deleteCourse(courseId: String, onSuccess: () -> Unit, onFailure: (Exception?) -> Unit) {
         viewModelScope.launch {
@@ -58,6 +125,26 @@ class CourseRepository(private val courseDao: CourseDao) {
                     onFailure(exception)
                 }
         }
+    }
+
+    private fun startCourseListener() {
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val courses = mutableListOf<CourseEntity>()
+                for (childSnapshot in snapshot.children) {
+                    val course = childSnapshot.getValue(CourseEntity::class.java)
+                    course?.let { courses.add(it) }
+                }
+                viewModelScope.launch {
+                    courseDao.insertCourses(courses)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle the read error (e.g., log the error)
+                println("Error reading courses: ${error.message}")
+            }
+        })
     }
 
     fun getCourseDetailsByCourseID(courseCode: String, onResult: (CourseEntity?) -> Unit) {
