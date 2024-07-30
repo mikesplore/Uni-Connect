2dd820a0ed9f8db764db54a747a384f9a0106ece
mikesplore <mikepremium8@gmail.com>
2024-07-30 18:37:40 +0300

    small fixes

--- app/src/main/java/com/mike/uniadmin/ui/theme/Theme.kt
+++ app/src/main/java/com/mike/uniadmin/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.mike.uniadmin.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -15,27 +14,27 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import com.mike.uniadmin.R
 
+
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkNavy,
+    secondary = DeepBlue,
+    tertiary = BrightBlue,
+    surface = LightGray,
+    onPrimary = Turquoise,
+    onSecondary = Black
+
+
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = LightBlue,
+    secondary = PaleBlue,
+    tertiary = Cyan,
+    surface = DarkGray,
+    onPrimary = SkyBlue,
+    onSecondary = White
 )
+
 
 val IndieFlower = FontFamily(
     Font(R.font.indieflower, FontWeight.Normal),
