072de544a1cb65d39b418261cf23e35e322b8ce8
mikesplore <mikepremium8@gmail.com>
2024-08-01 18:34:26 +0300

    small fixes

--- app/src/main/java/com/mike/uniadmin/attendance/Manage Attendance.kt
+++ app/src/main/java/com/mike/uniadmin/attendance/Manage Attendance.kt
@@ -14,10 +14,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
