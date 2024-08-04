ebe64f9c75d685acc140d2d98f469ac3dcc0f0fd
mikesplore <mikepremium8@gmail.com>
2024-08-04 18:40:43 +0300

    Improved performance by removing nullability checks

--- app/src/main/java/com/mike/uniadmin/dataModel/courses/Course.kt
+++ app/src/main/java/com/mike/uniadmin/dataModel/courses/Course.kt
@@ -7,30 +7,20 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "courses")
 data class CourseEntity(
     @PrimaryKey val courseCode: String,
-    val courseName: String? = null,
-    var visits: Int? = null,
-    var courseImageLink: String? = null
+    val courseName: String = "",
+    var visits: Int = 0,
+    var courseImageLink: String = ""
 
 ){
-    constructor(): this(
-        "",
-        "",
-        0,
-        ""
-    )
+    constructor(): this("", "", 0, "")
 }
 
 
 @Entity(tableName = "attendanceStates")
 data class AttendanceState(
     @PrimaryKey val courseID: String,
-    val courseName: String? = null,
+    val courseName: String = "",
     val state: Boolean = false
-)
-{
-    constructor(): this(
-        "",
-        "",
-        false
-    )
+){
+    constructor(): this("", "", false)
 }
