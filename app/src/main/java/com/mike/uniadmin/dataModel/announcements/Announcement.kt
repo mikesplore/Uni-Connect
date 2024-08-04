9e1d1413ca395f81b0e1e8d7ddb367e089a08546
mikesplore <mikepremium8@gmail.com>
2024-08-04 18:39:14 +0300

    Improved performance by removing nullability checks

--- app/src/main/java/com/mike/uniadmin/dataModel/announcements/Announcement.kt
+++ app/src/main/java/com/mike/uniadmin/dataModel/announcements/Announcement.kt
@@ -1,27 +1,18 @@
 package com.mike.uniadmin.dataModel.announcements
 
 import androidx.room.Entity
-import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-import com.mike.uniadmin.dataModel.users.UserEntity
 
 @Entity(tableName = "announcements")
 data class AnnouncementEntity(
     @PrimaryKey val id: String,
-    val date: String? = null,
-    val title: String? = null,
-    val description: String? = null,
-    val authorID: String? = null,
-    val authorName: String? = null,
-    val imageLink: String? = null
+    val date: String = "",
+    val title: String = "",
+    val description: String = "",
+    val authorID: String = "",
+    val authorName: String = "",
+    val imageLink: String = ""
 ){
-    constructor(): this(
-        "",
-        null,
-        null,
-        null,
-        null,
-        null
-    )
+    constructor(): this("", "", "", "", "", "", "")
 }
 
