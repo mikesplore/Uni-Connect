55713a6ec94c4de7f6d39440d7a6fb341a4eb294
mikesplore <mikepremium8@gmail.com>
2024-08-15 16:28:50 +0300

    improvements

--- app/src/main/java/com/mike/uniadmin/uniChat/GroupList.kt
+++ app/src/main/java/com/mike/uniadmin/uniChat/GroupList.kt
@@ -1,4 +1,4 @@
-package com.mike.uniadmin.chat
+package com.mike.uniadmin.uniChat
 
 import android.content.Context
 import android.widget.Toast
@@ -43,8 +43,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
