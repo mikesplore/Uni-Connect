aa3062c8d36ead69939ee2a3e515a4e907d0acd2
mikesplore <mikepremium8@gmail.com>
2024-08-18 17:50:21 +0300

    improvements

--- app/src/main/java/com/mike/uniadmin/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/mike/uniadmin/homeScreen/HomeScreen.kt
@@ -2,42 +2,18 @@ package com.mike.uniadmin.homeScreen
 
 import android.content.Context
 import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerDefaults
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Fingerprint
-import androidx.compose.material.icons.filled.LightMode
-import androidx.compose.material.icons.filled.Nightlight
-import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -48,32 +24,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
-import com.google.firebase.auth.FirebaseAuth
-import com.mike.uniadmin.DeviceTheme
 import com.mike.uniadmin.MainActivity
-import com.mike.uniadmin.attendance.ManageAttendanceScreen
 import com.mike.uniadmin.announcements.AnnouncementsScreen
 import com.mike.uniadmin.assignments.AssignmentScreen
-import com.mike.uniadmin.clearAllPreferences
+import com.mike.uniadmin.attendance.ManageAttendanceScreen
 import com.mike.uniadmin.dashboard.Dashboard
+import com.mike.uniadmin.dashboard.Sidebar
 import com.mike.uniadmin.dataModel.groupchat.ChatViewModel
 import com.mike.uniadmin.dataModel.groupchat.UniAdmin
-import com.mike.uniadmin.dataModel.users.UserEntity
-import com.mike.uniadmin.dataModel.users.UserStateEntity
 import com.mike.uniadmin.dataModel.users.UserViewModel
 import com.mike.uniadmin.dataModel.users.UserViewModelFactory
-import com.mike.uniadmin.model.MyDatabase
 import com.mike.uniadmin.model.MyDatabase.getUpdate
 import com.mike.uniadmin.model.Screen
 import com.mike.uniadmin.model.Update
@@ -103,8 +67,7 @@ fun HomeScreen(
         viewModel(factory = ChatViewModel.ChatViewModelFactory(chatRepository))
 
     val userRepository = remember { uniAdmin.userRepository }
-    val userViewModel: UserViewModel =
-        viewModel(factory = UserViewModelFactory(userRepository))
+    val userViewModel: UserViewModel = viewModel(factory = UserViewModelFactory(userRepository))
 
     // State observation
     val signedInUser by userViewModel.signedInUser.observeAsState()
@@ -119,7 +82,7 @@ fun HomeScreen(
     var update by remember { mutableStateOf(Update()) }
 
     val signedInUserLoading by userViewModel.isLoading.observeAsState()
-    val pagerState = rememberPagerState(pageCount = {screens.size})
+    val pagerState = rememberPagerState(pageCount = { screens.size })
 
 
     // Side effects
@@ -139,12 +102,22 @@ fun HomeScreen(
             }
         }
 
+
         // 3. Perform other data fetching and updates
         userViewModel.checkAllUserStatuses()
         chatViewModel.fetchGroups()
         userViewModel.fetchUsers()
         fetchedUserDetails?.id?.let { userId ->
             userViewModel.checkUserStateByID(userId)
+        }
+    }
+
+    LaunchedEffect(Sidebar.showSideBar.value) {
+        if (Sidebar.showSideBar.value) {
+            scope.launch {
+                drawerState.open()
+            }
+            Sidebar.showSideBar.value = false
         }
     }
 
@@ -156,39 +129,38 @@ fun HomeScreen(
                 showBottomSheet = false
             }
         }, containerColor = CC.primary(), sheetState = sheetState, content = {
-                ModalDrawerItem(
-                    context = context,
-                    navController = navController,
-                    userViewModel = userViewModel,
-                    chatViewModel = chatViewModel,
-                    activity = activity
-                )
-        })
-    }
-    CheckUpdate(context)
-    ModalNavigationDrawer(drawerState = drawerState, drawerContent = {
-            ModalNavigationDrawerItem(
-                drawerState = drawerState,
-                scope = coroutineScope,
+            ModalDrawerItem(
                 context = context,
                 navController = navController,
                 userViewModel = userViewModel,
                 chatViewModel = chatViewModel,
-                signedInUserLoading = signedInUserLoading,
-                signedInUser = signedInUser,
-                fetchedUserDetails = fetchedUserDetails,
-                showBottomSheet = {value -> showBottomSheet = value },
-                userStatus = userStatus,
-                update = update
-
+                activity = activity
             )
+        })
+    }
+    CheckUpdate(context)
+    ModalNavigationDrawer(drawerState = drawerState, drawerContent = {
+        ModalNavigationDrawerItem(
+            drawerState = drawerState,
+            scope = coroutineScope,
+            context = context,
+            navController = navController,
+            userViewModel = userViewModel,
+            chatViewModel = chatViewModel,
+            signedInUserLoading = signedInUserLoading,
+            signedInUser = signedInUser,
+            fetchedUserDetails = fetchedUserDetails,
+            showBottomSheet = { value -> showBottomSheet = value },
+            userStatus = userStatus,
+            update = update
+
+        )
 
 
     }) {
         Scaffold(
             bottomBar = {
-                BottomBar(
-                    context,
+                BottomBar(context,
                     screens,
                     pagerState,
                     coroutineScope,
@@ -196,10 +168,8 @@ fun HomeScreen(
                     userViewModel,
                     chatViewModel,
                     scope,
-                    showBottomSheet = { showBottomSheet = true }
-                )
-            },
-            containerColor = CC.primary()
+                    showBottomSheet = { showBottomSheet = true })
+            }, containerColor = CC.primary()
         ) { innerPadding ->
             Box(modifier = Modifier.fillMaxSize()) {
                 HorizontalPager(
