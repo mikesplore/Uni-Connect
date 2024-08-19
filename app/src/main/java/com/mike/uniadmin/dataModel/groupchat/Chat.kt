545dceaf1c1bb8cdb5f5ac8e83470a41873b5b9e
mikesplore <mikepremium8@gmail.com>
2024-08-20 01:38:58 +0300

    improvements

--- app/src/main/java/com/mike/uniadmin/dataModel/groupchat/Chat.kt
+++ app/src/main/java/com/mike/uniadmin/dataModel/groupchat/Chat.kt
@@ -48,16 +48,4 @@ data class GroupEntity(
 }
 
 
-class UniAdmin : Application() {
-    val database by lazy { AppDatabase.getDatabase(this) }
-    val chatRepository by lazy { ChatRepository(database.chatDao(), database.groupDao()) }
-    val messageRepository by lazy { MessageRepository(database.messageDao(), database.userDao()) }
-    val userRepository by lazy { UserRepository(database.userDao(), database.userStateDao(), database.accountDeletionDao(), database.userPreferencesDao(), database.databaseDao()) }
-    val announcementRepository by lazy { AnnouncementRepository(database.announcementsDao()) }
-    val notificationRepository by lazy { NotificationRepository(database.notificationDao()) }
-    val courseRepository by lazy {CourseRepository(database.courseDao(), database.attendanceStateDao())}
-    val courseAnnouncementRepository by lazy { CourseAnnouncementRepository(database.courseAnnouncementDao()) }
-    val courseAssignmentRepository by lazy { CourseAssignmentRepository(database.courseAssignmentDao()) }
-    val courseDetailRepository by lazy { CourseDetailRepository(database.courseDetailsDao()) }
-    val courseTimetableRepository by lazy { CourseTimetableRepository(database.courseTimetableDao()) }
-}
+
