8eedaac9b697164e6f67eefa1daf3d1d5e13949c
mikesplore <mikepremium8@gmail.com>
2024-08-20 21:51:27 +0300

    Introduced Programs

--- app/src/main/java/com/mike/uniadmin/backEnd/notifications/NotificationRepository.kt
+++ app/src/main/java/com/mike/uniadmin/backEnd/notifications/NotificationRepository.kt
@@ -1,10 +1,11 @@
-package com.mike.uniadmin.dataModel.notifications
+package com.mike.uniadmin.backEnd.notifications
 
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.mike.uniadmin.programs.ProgramCode
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,8 +13,8 @@ import kotlinx.coroutines.launch
 val viewModelScope = CoroutineScope(Dispatchers.Main)
 
 class NotificationRepository(private val notificationDao: NotificationDao) {
-
-    private val database = FirebaseDatabase.getInstance().reference.child("Notifications")
+    private val programCode = ProgramCode.programCode.value
+    private val database = FirebaseDatabase.getInstance().reference.child(programCode).child("Notifications")
     private val valueListeners = mutableMapOf<String, ValueEventListener>()
     private val childListeners = mutableMapOf<String, ChildEventListener>()
 
