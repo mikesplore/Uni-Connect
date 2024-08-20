746a90361376b5c1515211584f75c47da068c771
mikesplore <mikepremium8@gmail.com>
2024-08-20 21:54:11 +0300

    Introduced Programs

--- app/src/main/java/com/mike/uniadmin/homeScreen/BottomBar.kt
+++ app/src/main/java/com/mike/uniadmin/homeScreen/BottomBar.kt
@@ -31,8 +31,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.mike.uniadmin.dataModel.groupchat.ChatViewModel
-import com.mike.uniadmin.dataModel.users.UserViewModel
+import com.mike.uniadmin.backEnd.groupchat.ChatViewModel
+import com.mike.uniadmin.backEnd.users.UserViewModel
 import com.mike.uniadmin.model.Screen
 import com.mike.uniadmin.ui.theme.CommonComponents as CC
 import kotlinx.coroutines.CoroutineScope
