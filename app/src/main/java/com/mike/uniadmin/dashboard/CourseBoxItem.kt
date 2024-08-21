bf68159615badd1fdb60bed2eaf8540691b0afa9
mikesplore <mikepremium8@gmail.com>
2024-08-21 19:21:09 +0300

    some small changes

--- app/src/main/java/com/mike/uniadmin/dashboard/CourseBoxItem.kt
+++ app/src/main/java/com/mike/uniadmin/dashboard/CourseBoxItem.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
@@ -14,6 +15,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
@@ -34,6 +37,7 @@ import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.mike.uniadmin.courseResources.CourseName
 import com.mike.uniadmin.backEnd.courses.CourseEntity
+import com.mike.uniadmin.backEnd.courses.CourseViewModel
 import com.mike.uniadmin.ui.theme.CommonComponents as CC
 
 @Composable
@@ -49,16 +53,18 @@ fun CourseBox(
                 model = course.courseImageLink,
                 contentDescription = course.courseName,
                 modifier = Modifier
-                    .clip(RoundedCornerShape(16.dp, 16.dp))
+                    .clip(RoundedCornerShape(16.dp, 16.dp, 0.dp, 0.dp))
                     .fillMaxSize(),
                 alignment = Alignment.Center,
                 contentScale = ContentScale.Crop
             )
         },
         bodyContent = {
-            Text(course.courseCode,
+            Text(
+                course.courseCode,
                 style = CC.descriptionTextStyle(context),
-                modifier = Modifier.padding(start = 10.dp))
+                modifier = Modifier.padding(start = 10.dp)
+            )
             Text(
                 course.courseName,
                 style = CC.titleTextStyle(context)
@@ -108,14 +114,21 @@ fun LoadingCourseBox() {
         imageContent = {
             CC.ColorProgressIndicator(
                 modifier = Modifier
-                    .clip(RoundedCornerShape(16.dp, 16.dp, 0.dp, 0.dp))
+                    .clip(RoundedCornerShape(16.dp))
                     .fillMaxSize()
             )
         },
         bodyContent = {
-            LoadingPlaceholder(modifier = Modifier.height(20.dp).width(100.dp))
-            LoadingPlaceholder(modifier = Modifier.height(25.dp).padding(horizontal = 10.dp))
-            LoadingPlaceholder(modifier = Modifier.height(25.dp).width(100.dp))
+            LoadingPlaceholder(modifier = Modifier
+                .height(20.dp)
+                .fillMaxWidth(0.5f)) // Adjusted to a fraction of width
+            LoadingPlaceholder(modifier = Modifier
+                .height(25.dp)
+                .padding(horizontal = 10.dp)
+                .fillMaxWidth())
+            LoadingPlaceholder(modifier = Modifier
+                .height(25.dp)
+                .fillMaxWidth(0.5f)) // Adjusted to a fraction of width
         }
     )
 }
@@ -125,32 +138,68 @@ fun BaseCourseBox(
     imageContent: @Composable BoxScope.() -> Unit,
     bodyContent: @Composable ColumnScope.() -> Unit
 ) {
-    Column(
-        modifier = Modifier
-            .shadow(
-                elevation = 4.dp,
-                shape = RoundedCornerShape(16.dp)
-            )
-            .width(200.dp)
-            .height(230.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxHeight(0.4f)
-                .background(CC.extraColor2(), RoundedCornerShape(16.dp, 16.dp, 0.dp, 0.dp))
-                .fillMaxWidth(),
-            content = imageContent
-        )
+    BoxWithConstraints {
+        // Dynamically calculate width and height based on screen size
+        val boxWidth = maxWidth * 0.95f  // 40% of available width
+        val boxHeight = boxWidth * 1.25f // Maintain an aspect ratio
+
         Column(
             modifier = Modifier
-                .fillMaxHeight()
-                .background(CC.extraColor1(), RoundedCornerShape(0.dp, 0.dp, 16.dp, 16.dp))
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.SpaceBetween,
-            content = bodyContent
-        )
+                .shadow(
+                    elevation = 4.dp,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .width(boxWidth)
+                .height(boxHeight),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight(0.4f)
+                    .background(CC.extraColor2(), RoundedCornerShape(16.dp, 16.dp, 0.dp, 0.dp))
+                    .fillMaxWidth(),
+                content = imageContent
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .background(CC.extraColor1(), RoundedCornerShape(0.dp, 0.dp, 16.dp, 16.dp))
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.SpaceBetween,
+                content = bodyContent
+            )
+        }
     }
 }
 
+@Composable
+fun CourseBoxList(courses: List<CourseEntity>, context: Context, navController: NavController, courseViewModel: CourseViewModel){
+    BoxWithConstraints {
+        val screenWidth = maxWidth
+        val itemWidth = screenWidth * 0.4f // Each item takes 40% of the screen width
+
+        // Set a minimum and maximum width for the items
+        val adaptiveItemWidth = itemWidth.coerceIn(minimumValue = 200.dp, maximumValue = 250.dp)
+        LazyRow(
+            modifier = Modifier
+                .padding(horizontal = 10.dp)
+                .fillMaxWidth(),
+        ) {
+            items(courses) { course -> // Use the sorted list
+                Box(
+                    modifier = Modifier.width(adaptiveItemWidth) // Apply the adaptive width
+                ) {
+                CourseBox(course, context, navController, onClicked = {
+                    courseViewModel.saveCourse(
+                        course.copy(
+                            visits = course.visits.plus(
+                                1
+                            )
+                        )
+                    )
+                })}
+            }
+        }
+    }
+}
 
