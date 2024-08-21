ed1015263232f1350f28f1a7001f40bbb95c6d8f
mikesplore <mikepremium8@gmail.com>
2024-08-21 19:20:38 +0300

    some small changes

--- app/src/main/java/com/mike/uniadmin/ui/theme/Color.kt
+++ app/src/main/java/com/mike/uniadmin/ui/theme/Color.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 
 /*⚠️
-    The names use here are not the actual color names
+    The color names use here are not the actual names
     I was changing the codes to find the best palette for the app
   ⚠️
 */
@@ -12,7 +12,7 @@ import androidx.compose.ui.graphics.Color
 
 //light theme
 val LightBlue = Color(0xFFDCEEFB)
-val PaleBlue = Color(0xFF117A65)
+val PaleBlue = Color(0xFF7D8ABC)
 val Cyan = Color(0xFF2980B9)
 val DarkGray = Color(0xFF212121)
 val SkyBlue = Color(0xFFBBDEFB)
@@ -22,7 +22,7 @@ val White = Color(0xFF1B4F72)
 //dark theme
 val DarkNavy = Color(0xFF15202B)
 val DeepBlue = Color(0xFF1B4F72)
-val BrightBlue = Color(0xFF117A65)
+val BrightBlue = Color(0xFF7D8ABC)
 val LightGray = Color(0xFFE1E8ED)
 val Turquoise = Color(0xFF1C2833)
 val Black = Color(0xFF2980B9)
