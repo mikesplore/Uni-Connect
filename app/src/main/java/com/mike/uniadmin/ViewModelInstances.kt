61e5ac1bc437b3935c5b869bc6954c2908506fac
mikesplore <mikepremium8@gmail.com>
2024-08-22 13:52:28 +0300

    some small changes

--- app/src/main/java/com/mike/uniadmin/ViewModelInstances.kt
+++ app/src/main/java/com/mike/uniadmin/ViewModelInstances.kt
@@ -6,39 +6,39 @@ import androidx.compose.runtime.remember
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.mike.uniadmin.backEnd.announcements.AnnouncementViewModel
 import com.mike.uniadmin.backEnd.announcements.AnnouncementViewModelFactory
-import com.mike.uniadmin.backEnd.coursecontent.courseannouncements.CourseAnnouncementViewModel
-import com.mike.uniadmin.backEnd.coursecontent.courseannouncements.CourseAnnouncementViewModelFactory
-import com.mike.uniadmin.backEnd.coursecontent.courseassignments.CourseAssignmentViewModel
-import com.mike.uniadmin.backEnd.coursecontent.courseassignments.CourseAssignmentViewModelFactory
-import com.mike.uniadmin.backEnd.coursecontent.coursedetails.CourseDetailViewModel
-import com.mike.uniadmin.backEnd.coursecontent.coursedetails.CourseDetailViewModelFactory
-import com.mike.uniadmin.backEnd.coursecontent.coursetimetable.CourseTimetableViewModel
-import com.mike.uniadmin.backEnd.coursecontent.coursetimetable.CourseTimetableViewModelFactory
-import com.mike.uniadmin.backEnd.courses.CourseViewModel
-import com.mike.uniadmin.backEnd.courses.CourseViewModelFactory
-import com.mike.uniadmin.backEnd.groupchat.ChatViewModel
+import com.mike.uniadmin.backEnd.moduleContent.moduleAnnouncements.ModuleAnnouncementViewModel
+import com.mike.uniadmin.backEnd.moduleContent.moduleAnnouncements.ModuleAnnouncementViewModelFactory
+import com.mike.uniadmin.backEnd.moduleContent.moduleAssignments.ModuleAssignmentViewModel
+import com.mike.uniadmin.backEnd.moduleContent.moduleAssignments.ModuleAssignmentViewModelFactory
+import com.mike.uniadmin.backEnd.moduleContent.moduleDetails.ModuleDetailViewModel
+import com.mike.uniadmin.backEnd.moduleContent.moduleDetails.ModuleDetailViewModelFactory
+import com.mike.uniadmin.backEnd.moduleContent.moduleTimetable.ModuleTimetableViewModel
+import com.mike.uniadmin.backEnd.moduleContent.moduleTimetable.ModuleTimetableViewModelFactory
+import com.mike.uniadmin.backEnd.modules.ModuleViewModel
+import com.mike.uniadmin.backEnd.modules.ModuleViewModelFactory
+import com.mike.uniadmin.backEnd.groupchat.GroupChatViewModel
 import com.mike.uniadmin.backEnd.notifications.NotificationViewModel
-import com.mike.uniadmin.backEnd.programs.ProgramViewModel
-import com.mike.uniadmin.backEnd.programs.ProgramViewModelFactory
-import com.mike.uniadmin.backEnd.userchat.MessageViewModel
+import com.mike.uniadmin.backEnd.modules.CourseViewModel
+import com.mike.uniadmin.backEnd.modules.CourseViewModelFactory
+import com.mike.uniadmin.backEnd.userchat.UserGroupChatViewModel
 import com.mike.uniadmin.backEnd.users.UserViewModel
 import com.mike.uniadmin.backEnd.users.UserViewModelFactory
 import com.mike.uniadmin.localDatabase.UniAdmin
 
 //messageViewModel
 @Composable
-fun getMessageViewModel(context: Context): MessageViewModel {
+fun getUserGroupChatViewModel(context: Context): UserGroupChatViewModel {
     val application = context.applicationContext as UniAdmin
     val messageRepository = application.messageRepository
-    return viewModel(factory = MessageViewModel.MessageViewModelFactory(messageRepository))
+    return viewModel(factory = UserGroupChatViewModel.MessageViewModelFactory(messageRepository))
 }
 
 //chatViewModel
 @Composable
-fun getChatViewModel(context: Context): ChatViewModel {
+fun getGroupChatViewModel(context: Context): GroupChatViewModel {
     val application = context.applicationContext as UniAdmin
     val chatRepository = application.chatRepository
-    return viewModel(factory = ChatViewModel.ChatViewModelFactory(chatRepository))
+    return viewModel(factory = GroupChatViewModel.GroupChatViewModelFactory(chatRepository))
 }
 
 //userViewModel
@@ -50,44 +50,44 @@ fun getUserViewModel(context: Context): UserViewModel {
 }
 
 
-//CourseViewModel
+//ModuleViewModel
 @Composable
-fun getCourseViewModel(context: Context): CourseViewModel {
-    val courseResource = context.applicationContext as UniAdmin
-    val courseRepository = remember { courseResource.courseRepository }
-    return viewModel(factory = CourseViewModelFactory(courseRepository))
+fun getModuleViewModel(context: Context): ModuleViewModel {
+    val moduleResource = context.applicationContext as UniAdmin
+    val moduleRepository = remember { moduleResource.moduleRepository }
+    return viewModel(factory = ModuleViewModelFactory(moduleRepository))
 }
 
-//CourseAnnouncementViewModel
+//ModuleAnnouncementViewModel
 @Composable
-fun getCourseAnnouncementViewModel(context: Context): CourseAnnouncementViewModel {
-    val courseResource = context.applicationContext as UniAdmin
-    val courseAnnouncementRepository = remember { courseResource.courseAnnouncementRepository }
-    return viewModel(factory = CourseAnnouncementViewModelFactory(courseAnnouncementRepository))
+fun getModuleAnnouncementViewModel(context: Context): ModuleAnnouncementViewModel {
+    val moduleResource = context.applicationContext as UniAdmin
+    val moduleAnnouncementRepository = remember { moduleResource.moduleAnnouncementRepository }
+    return viewModel(factory = ModuleAnnouncementViewModelFactory(moduleAnnouncementRepository))
 }
 
-//CourseAssignmentViewModel
+//ModuleAssignmentViewModel
 @Composable
-fun getCourseAssignmentViewModel(context: Context): CourseAssignmentViewModel {
-    val courseResource = context.applicationContext as UniAdmin
-    val courseAssignmentRepository = remember { courseResource.courseAssignmentRepository }
-    return viewModel(factory = CourseAssignmentViewModelFactory(courseAssignmentRepository))
+fun getModuleAssignmentViewModel(context: Context): ModuleAssignmentViewModel {
+    val moduleResource = context.applicationContext as UniAdmin
+    val moduleAssignmentRepository = remember { moduleResource.moduleAssignmentRepository }
+    return viewModel(factory = ModuleAssignmentViewModelFactory(moduleAssignmentRepository))
 }
 
-//CourseDetailViewModel
+//ModuleDetailViewModel
 @Composable
-fun getCourseDetailViewModel(context: Context): CourseDetailViewModel {
-    val courseResource = context.applicationContext as UniAdmin
-    val courseDetailRepository = remember { courseResource.courseDetailRepository }
-    return viewModel(factory = CourseDetailViewModelFactory(courseDetailRepository))
+fun getModuleDetailViewModel(context: Context): ModuleDetailViewModel {
+    val moduleResource = context.applicationContext as UniAdmin
+    val moduleDetailRepository = remember { moduleResource.moduleDetailRepository }
+    return viewModel(factory = ModuleDetailViewModelFactory(moduleDetailRepository))
 }
 
-//CourseTimetableViewModel
+//ModuleTimetableViewModel
 @Composable
-fun getCourseTimetableViewModel(context: Context): CourseTimetableViewModel {
-    val courseResource = context.applicationContext as UniAdmin
-    val courseTimetableRepository = remember { courseResource.courseTimetableRepository }
-    return viewModel(factory = CourseTimetableViewModelFactory(courseTimetableRepository))
+fun getModuleTimetableViewModel(context: Context): ModuleTimetableViewModel {
+    val moduleResource = context.applicationContext as UniAdmin
+    val moduleTimetableRepository = remember { moduleResource.moduleTimetableRepository }
+    return viewModel(factory = ModuleTimetableViewModelFactory(moduleTimetableRepository))
 }
 
 //AnnouncementViewModel
@@ -106,11 +106,11 @@ fun getNotificationViewModel(context: Context): NotificationViewModel {
     return viewModel(factory = NotificationViewModel.NotificationViewModelFactory(notificationRepository))
 }
 
-//ProgramViewModel
+//CourseViewModel
 @Composable
-fun getProgramViewModel(context: Context): ProgramViewModel {
-    val programResource = context.applicationContext as UniAdmin
-    val programRepository = remember { programResource.programRepository }
-    return viewModel(factory = ProgramViewModelFactory(programRepository))
+fun getCourseViewModel(context: Context): CourseViewModel {
+    val courseResource = context.applicationContext as UniAdmin
+    val courseRepository = remember { courseResource.courseRepository }
+    return viewModel(factory = CourseViewModelFactory(courseRepository))
 }
 
