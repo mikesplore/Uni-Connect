428a0a729582961d9e80e2400da5809b6af71b76
mikesplore <mikepremium8@gmail.com>
2024-08-22 14:00:51 +0300

    some small changes

---
+++ app/src/main/java/com/mike/uniadmin/backEnd/moduleContent/moduleDetails/CourseDetailsDao.kt
@@ -0,0 +1,23 @@
+package com.mike.uniadmin.backEnd.moduleContent.moduleDetails
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.mike.uniadmin.backEnd.moduleContent.moduleDetails.ModuleDetail
+import com.mike.uniadmin.backEnd.modules.ModuleEntity
+
+@Dao
+interface ModuleDetailDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertModuleDetail(moduleDetail: ModuleDetail)
+
+    @Query("SELECT * FROM moduleDetails WHERE moduleCode = :moduleId LIMIT 1")
+    suspend fun getModuleDetail(moduleId: String): ModuleDetail?
+
+    @Query("SELECT moduleName, moduleCode, moduleImageLink, visits FROM modules WHERE moduleCode = :moduleId")
+    suspend fun getModuleDetailsByID(moduleId: String): ModuleEntity?
+
+    @Query("DELETE FROM moduleDetails WHERE detailID = :detailID")
+    suspend fun deleteModuleDetail(detailID: String)
+}
