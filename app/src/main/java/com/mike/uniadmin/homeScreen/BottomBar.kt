73600384726419b7fb09faab9eb84ebd00816033
mikesplore <mikepremium8@gmail.com>
2024-08-22 14:01:45 +0300

    some small changes

--- app/src/main/java/com/mike/uniadmin/homeScreen/BottomBar.kt
+++ app/src/main/java/com/mike/uniadmin/homeScreen/BottomBar.kt
@@ -31,7 +31,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.mike.uniadmin.backEnd.groupchat.ChatViewModel
+import com.mike.uniadmin.backEnd.groupchat.GroupChatViewModel
 import com.mike.uniadmin.backEnd.users.UserViewModel
 import com.mike.uniadmin.model.Screen
 import com.mike.uniadmin.ui.theme.CommonComponents as CC
@@ -47,7 +47,7 @@ fun BottomBar(
     coroutineScope: CoroutineScope,
     drawerState: DrawerState,
     userViewModel: UserViewModel,
-    chatViewModel: ChatViewModel,
+    chatViewModel: GroupChatViewModel,
     scope: CoroutineScope,
     showBottomSheet:(Boolean) -> Unit
 ){
