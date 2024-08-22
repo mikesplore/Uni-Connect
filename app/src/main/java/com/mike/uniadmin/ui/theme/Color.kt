70e610b72199c1d1595f004c008897a9e08d482e
mikesplore <mikepremium8@gmail.com>
2024-08-22 21:45:47 +0300

    some small changes

--- app/src/main/java/com/mike/uniadmin/ui/theme/Color.kt
+++ app/src/main/java/com/mike/uniadmin/ui/theme/Color.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.graphics.Color
 
 //light theme
 val LightBlue = Color(0xFFDCEEFB)
-val PaleBlue = Color(0xFF7D8ABC)
+val PaleBlue = Color(0xFF86B6F6)
 val Cyan = Color(0xFF2980B9)
 val DarkGray = Color(0xFF212121)
 val SkyBlue = Color(0xFFBBDEFB)
@@ -22,7 +22,7 @@ val White = Color(0xFF1B4F72)
 //dark theme
 val DarkNavy = Color(0xFF15202B)
 val DeepBlue = Color(0xFF1B4F72)
-val BrightBlue = Color(0xFF7D8ABC)
+val BrightBlue = Color(0xFF86B6F6)
 val LightGray = Color(0xFFE1E8ED)
 val Turquoise = Color(0xFF1C2833)
 val Black = Color(0xFF2980B9)
