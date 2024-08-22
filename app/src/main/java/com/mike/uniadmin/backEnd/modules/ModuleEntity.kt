47e2f7bb83f81554b3c629d5982b1d06e28a467e
mikesplore <mikepremium8@gmail.com>
2024-08-22 13:59:05 +0300

    some small changes

---
+++ app/src/main/java/com/mike/uniadmin/backEnd/modules/ModuleEntity.kt
@@ -0,0 +1,26 @@
+package com.mike.uniadmin.backEnd.modules
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "modules")
+data class ModuleEntity(
+    @PrimaryKey val moduleCode: String,
+    val moduleName: String = "",
+    var visits: Int = 0,
+    var moduleImageLink: String = ""
+
+){
+    constructor(): this("", "", 0, "")
+}
+
+
+@Entity(tableName = "attendanceStates")
+data class AttendanceState(
+    @PrimaryKey val moduleID: String,
+    val moduleName: String = "",
+    val state: Boolean = false
+){
+    constructor(): this("", "", false)
+}
