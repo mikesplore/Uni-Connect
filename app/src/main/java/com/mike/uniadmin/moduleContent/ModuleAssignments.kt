a815ffd3a9a910969ce48ba80efc7e4cc9dbf9f0
mikesplore <mikepremium8@gmail.com>
2024-08-22 22:15:21 +0300

    some small changes

--- app/src/main/java/com/mike/uniadmin/moduleContent/ModuleAssignments.kt
+++ app/src/main/java/com/mike/uniadmin/moduleContent/ModuleAssignments.kt
@@ -57,7 +57,7 @@ import coil.compose.AsyncImage
 import com.mike.uniadmin.backEnd.moduleContent.moduleAssignments.ModuleAssignment
 import com.mike.uniadmin.backEnd.moduleContent.moduleAssignments.ModuleAssignmentViewModel
 import com.mike.uniadmin.backEnd.users.UserViewModel
-import com.mike.uniadmin.model.MyDatabase
+import com.mike.uniadmin.helperFunctions.MyDatabase
 import com.mike.uniadmin.ui.theme.CommonComponents as CC
 import java.text.DateFormat
 import java.text.SimpleDateFormat
