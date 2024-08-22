e12542e53526c5df0bef86f5563acdb0883bab73
mikesplore <mikepremium8@gmail.com>
2024-08-22 23:17:49 +0300

    some small changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 20
-        versionName = "1.2.6"
+        versionName = "1.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
