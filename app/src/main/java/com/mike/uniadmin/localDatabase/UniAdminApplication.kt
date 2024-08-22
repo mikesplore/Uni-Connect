62293210734dae4c8b056144fa484a44d26e63de
mikesplore <mikepremium8@gmail.com>
2024-08-22 14:35:50 +0300

    some small changes

--- app/src/main/java/com/mike/uniadmin/localDatabase/UniAdminApplication.kt
+++ app/src/main/java/com/mike/uniadmin/localDatabase/UniAdminApplication.kt
@@ -1,6 +1,7 @@
 package com.mike.uniadmin.localDatabase
 
 import android.app.Application
+import androidx.compose.ui.tooling.data.Group
 import com.mike.uniadmin.backEnd.announcements.AnnouncementRepository
 import com.mike.uniadmin.backEnd.moduleContent.moduleAnnouncements.ModuleAnnouncementRepository
 import com.mike.uniadmin.backEnd.moduleContent.moduleAssignments.ModuleAssignmentRepository
@@ -10,14 +11,14 @@ import com.mike.uniadmin.backEnd.modules.ModuleRepository
 import com.mike.uniadmin.backEnd.groupchat.GroupChatRepository
 import com.mike.uniadmin.backEnd.notifications.NotificationRepository
 import com.mike.uniadmin.backEnd.programs.CourseRepository
-import com.mike.uniadmin.backEnd.userchat.UserGroupChatRepository
+import com.mike.uniadmin.backEnd.userchat.UserChatRepository
 import com.mike.uniadmin.backEnd.users.UserRepository
 
 
 class UniAdmin : Application() {
     val database by lazy { AppDatabase.getDatabase(this) }
-    val chatRepository by lazy { GroupChatRepository(database.chatDao(), database.groupDao()) }
-    val messageRepository by lazy { UserGroupChatRepository(database.messageDao()) }
+    val groupChatRepository by lazy { GroupChatRepository(database.chatDao(), database.groupDao()) }
+    val userChatRepository by lazy { UserChatRepository(database.messageDao()) }
     val userRepository by lazy { UserRepository(database.userDao(), database.userStateDao(), database.accountDeletionDao(), database.userPreferencesDao(), database.databaseDao()) }
     val announcementRepository by lazy { AnnouncementRepository(database.announcementsDao()) }
     val notificationRepository by lazy { NotificationRepository(database.notificationDao()) }
