8b67ff428be838b73f9f454c739adb5ab56a3cd8
mikesplore <mikepremium8@gmail.com>
2024-08-22 13:58:01 +0300

    some small changes

--- app/src/main/java/com/mike/uniadmin/localDatabase/UniAdminApplication.kt
+++ app/src/main/java/com/mike/uniadmin/localDatabase/UniAdminApplication.kt
@@ -2,30 +2,30 @@ package com.mike.uniadmin.localDatabase
 
 import android.app.Application
 import com.mike.uniadmin.backEnd.announcements.AnnouncementRepository
-import com.mike.uniadmin.backEnd.coursecontent.courseannouncements.CourseAnnouncementRepository
-import com.mike.uniadmin.backEnd.coursecontent.courseassignments.CourseAssignmentRepository
-import com.mike.uniadmin.backEnd.coursecontent.coursedetails.CourseDetailRepository
-import com.mike.uniadmin.backEnd.coursecontent.coursetimetable.CourseTimetableRepository
-import com.mike.uniadmin.backEnd.courses.CourseRepository
-import com.mike.uniadmin.backEnd.groupchat.ChatRepository
+import com.mike.uniadmin.backEnd.moduleContent.moduleAnnouncements.ModuleAnnouncementRepository
+import com.mike.uniadmin.backEnd.moduleContent.moduleAssignments.ModuleAssignmentRepository
+import com.mike.uniadmin.backEnd.moduleContent.moduleDetails.ModuleDetailRepository
+import com.mike.uniadmin.backEnd.moduleContent.moduleTimetable.ModuleTimetableRepository
+import com.mike.uniadmin.backEnd.modules.ModuleRepository
+import com.mike.uniadmin.backEnd.groupchat.GroupChatRepository
 import com.mike.uniadmin.backEnd.notifications.NotificationRepository
-import com.mike.uniadmin.backEnd.programs.ProgramRepository
-import com.mike.uniadmin.backEnd.userchat.MessageRepository
+import com.mike.uniadmin.backEnd.programs.CourseRepository
+import com.mike.uniadmin.backEnd.userchat.UserGroupChatRepository
 import com.mike.uniadmin.backEnd.users.UserRepository
 
 
 class UniAdmin : Application() {
     val database by lazy { AppDatabase.getDatabase(this) }
-    val chatRepository by lazy { ChatRepository(database.chatDao(), database.groupDao()) }
-    val messageRepository by lazy { MessageRepository(database.messageDao()) }
+    val chatRepository by lazy { GroupChatRepository(database.chatDao(), database.groupDao()) }
+    val messageRepository by lazy { UserGroupChatRepository(database.messageDao()) }
     val userRepository by lazy { UserRepository(database.userDao(), database.userStateDao(), database.accountDeletionDao(), database.userPreferencesDao(), database.databaseDao()) }
     val announcementRepository by lazy { AnnouncementRepository(database.announcementsDao()) }
     val notificationRepository by lazy { NotificationRepository(database.notificationDao()) }
-    val courseRepository by lazy { CourseRepository(database.courseDao(), database.attendanceStateDao()) }
-    val courseAnnouncementRepository by lazy { CourseAnnouncementRepository(database.courseAnnouncementDao()) }
-    val courseAssignmentRepository by lazy { CourseAssignmentRepository(database.courseAssignmentDao()) }
-    val courseDetailRepository by lazy { CourseDetailRepository(database.courseDetailsDao()) }
-    val courseTimetableRepository by lazy { CourseTimetableRepository(database.courseTimetableDao()) }
-    val programRepository by lazy { ProgramRepository(database.programDao(), database.programStateDao()) }
+    val moduleRepository by lazy { ModuleRepository(database.moduleDao(), database.attendanceStateDao()) }
+    val moduleAnnouncementRepository by lazy { ModuleAnnouncementRepository(database.moduleAnnouncementDao()) }
+    val moduleAssignmentRepository by lazy { ModuleAssignmentRepository(database.moduleAssignmentDao()) }
+    val moduleDetailRepository by lazy { ModuleDetailRepository(database.moduleDetailsDao()) }
+    val moduleTimetableRepository by lazy { ModuleTimetableRepository(database.moduleTimetableDao()) }
+    val courseRepository by lazy { CourseRepository(database.courseDao(), database.courseStateDao()) }
 
 }
