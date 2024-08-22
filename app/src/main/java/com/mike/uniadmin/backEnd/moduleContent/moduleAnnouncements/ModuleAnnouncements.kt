7f18a69bb71d18718951a968b0a146d15484ed4c
mikesplore <mikepremium8@gmail.com>
2024-08-22 13:59:39 +0300

    some small changes

--- app/src/main/java/com/mike/uniadmin/backEnd/moduleContent/moduleAnnouncements/ModuleAnnouncements.kt
+++ app/src/main/java/com/mike/uniadmin/backEnd/moduleContent/moduleAnnouncements/ModuleAnnouncements.kt
@@ -1,12 +1,12 @@
-package com.mike.uniadmin.backEnd.coursecontent.courseannouncements
+package com.mike.uniadmin.backEnd.moduleContent.moduleAnnouncements
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "courseAnnouncements")
-data class CourseAnnouncement(
+@Entity(tableName = "moduleAnnouncements")
+data class ModuleAnnouncement(
   @PrimaryKey val announcementID: String,
-  val courseID: String = "",
+  val moduleID: String = "",
   val title: String = "",
   val content: String = "",
   val date: String = "",
