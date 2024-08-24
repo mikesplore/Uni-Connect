4d645b351b6da25442048598d0ab69bc2f0ee419
mikesplore <mikepremium8@gmail.com>
2024-08-24 11:12:11 +0300

    Introducing Account Types

--- app/src/main/java/com/mike/uniadmin/backEnd/attendance/AttendanceViewModel.kt
+++ app/src/main/java/com/mike/uniadmin/backEnd/attendance/AttendanceViewModel.kt
@@ -2,10 +2,17 @@ package com.mike.uniadmin.backEnd.attendance
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
-class AttendanceViewModel (private val attendanceRepository: AttendanceRepository){
+
+class AttendanceViewModel (private val attendanceRepository: AttendanceRepository): ViewModel() {
     private val _attendance = MutableLiveData<List<AttendanceEntity>>()
     val attendance: LiveData<List<AttendanceEntity>> get() = _attendance
+
+    init {
+        syncAttendanceUpdates()
+    }
 
     fun getAttendanceForStudent(studentId: String, courseId: String) {
         attendanceRepository.getAttendanceForStudent(studentId, courseId) { attendanceList ->
@@ -13,10 +20,22 @@ class AttendanceViewModel (private val attendanceRepository: AttendanceRepositor
         }
     }
 
-    fun insertAttendance(attendance: AttendanceEntity) {
+    fun signAttendance(attendance: AttendanceEntity) {
         attendanceRepository.insertAttendance(attendance)
     }
 
+    private fun syncAttendanceUpdates() {
+        attendanceRepository.syncAttendanceUpdates()
+    }
 
+    class AttendanceViewModelFactory(private val repository: AttendanceRepository) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(AttendanceViewModel::class.java)) {
+                return AttendanceViewModel(repository) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class for Chats")
+        }
+    }
 
 }
