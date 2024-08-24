19b97d47d5d5927ea6bdb6ae25a6144b79644df3
mikesplore <mikepremium8@gmail.com>
2024-08-24 11:11:45 +0300

    Introducing Account Types

--- app/src/main/java/com/mike/uniadmin/SplashScreen.kt
+++ app/src/main/java/com/mike/uniadmin/SplashScreen.kt
@@ -1,7 +1,6 @@
 package com.mike.uniadmin
 
 import android.content.Context
-import android.util.Log
 import android.view.animation.OvershootInterpolator
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
@@ -11,15 +10,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -37,16 +32,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.mike.uniadmin.authentication.UserType
-import kotlinx.coroutines.delay
 import com.mike.uniadmin.ui.theme.CommonComponents as CC
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SplashScreen(navController: NavController, context: Context) {
     var startAnimation by remember { mutableStateOf(false) }
-    var showBottomSheet by remember { mutableStateOf(false) }
 
     val scale by animateFloatAsState(
         targetValue = if (startAnimation) 1f else 0.8f,
@@ -54,53 +45,23 @@ fun SplashScreen(navController: NavController, context: Context) {
         label = ""
     )
 
-    val userType = UniAdminPreferences.userType.value
     val email = UniAdminPreferences.userEmail.value
     val courseCode = UniAdminPreferences.courseCode.value
 
     val destination = when {
-        userType.isEmpty() -> null // Show bottom sheet if userType is not selected
-        email.isEmpty() -> "login" // User type selected but email is empty, go to login
-        courseCode.isEmpty() -> "courses" // Email is set but courseCode is empty, go to courses
-        else -> "homeScreen" // All data is present, go to homeScreen
+        email.isEmpty() -> "login"
+        courseCode.isEmpty() -> "courses"
+        else -> "homeScreen"
     }
 
-    LaunchedEffect(userType) {
-        if (userType.isEmpty()) {
-            delay(1000) // Delay to complete the splash animation first
-            showBottomSheet = true // Show the bottom sheet if userType is empty
-        } else if (destination != null) {
-            navController.navigate(destination) {
-                popUpTo("splashScreen") { inclusive = true }
-            }
+    LaunchedEffect(Unit) {
+        navController.navigate(destination) {
+            popUpTo("splashScreen") { inclusive = true }
         }
     }
 
     LaunchedEffect(Unit) {
         startAnimation = true
-    }
-
-    if (email.isNotEmpty()) {
-        Log.d("Found User", "User found in the database $email")
-    } else {
-        Log.e("Found User", "No User found in the database")
-    }
-
-    if (showBottomSheet) {
-        val maxHeight = LocalContext.current.resources.displayMetrics.heightPixels.dp / 2
-        ModalBottomSheet(
-            onDismissRequest = { /* Prevent dismissing until a type is selected */ },
-            containerColor = CC.primary(),
-            modifier = Modifier.heightIn(max = maxHeight * 0.4f) // Adjusted height
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                UserType(context, navController)
-            }
-        }
     }
 
     Box(
