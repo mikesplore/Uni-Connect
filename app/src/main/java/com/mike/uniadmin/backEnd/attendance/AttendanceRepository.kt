096f7b724ac3e1d9c92496671db377a3249feae5
mikesplore <mikepremium8@gmail.com>
2024-08-27 21:04:39 +0300

    Few Fixes

--- app/src/main/java/com/mike/uniadmin/backEnd/attendance/AttendanceRepository.kt
+++ app/src/main/java/com/mike/uniadmin/backEnd/attendance/AttendanceRepository.kt
@@ -3,6 +3,7 @@ package com.mike.uniadmin.backEnd.attendance
 import android.util.Log
 import com.google.firebase.database.*
 import com.mike.uniadmin.UniAdminPreferences
+import com.mike.uniadmin.backEnd.announcements.uniConnectScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -10,7 +11,6 @@ import kotlinx.coroutines.launch
 class AttendanceRepository(private val attendanceDao: AttendanceDao) {
     private val courseCode = UniAdminPreferences.courseCode.value
     private val database = FirebaseDatabase.getInstance().reference.child(courseCode).child("Attendance")
-    private val viewModelScope = CoroutineScope(Dispatchers.Main)
 
     // Function to get attendance for a specific student and course
     fun getAttendanceForStudent(
@@ -18,7 +18,7 @@ class AttendanceRepository(private val attendanceDao: AttendanceDao) {
         courseId: String,
         onResult: (List<AttendanceEntity>) -> Unit
     ) {
-        viewModelScope.launch {
+        uniConnectScope.launch {
             val localAttendance = attendanceDao.getAttendanceForStudent(studentId, courseId)
             if (localAttendance.isNotEmpty()) {
                 onResult(localAttendance)
@@ -37,7 +37,7 @@ class AttendanceRepository(private val attendanceDao: AttendanceDao) {
                     }
 
                     if (attendanceList.isNotEmpty()) {
-                        viewModelScope.launch {
+                        uniConnectScope.launch {
                             attendanceDao.insertAllAttendance(attendanceList)
                         }
                         onResult(attendanceList)
@@ -52,7 +52,7 @@ class AttendanceRepository(private val attendanceDao: AttendanceDao) {
     }
 
     fun signAttendance(attendance: AttendanceEntity, success: (Boolean) -> Unit) {
-        viewModelScope.launch {
+        uniConnectScope.launch {
             // Insert into Room database
             attendanceDao.insertAttendance(attendance)
             // Update Firebase under the specific course and student
@@ -99,7 +99,7 @@ class AttendanceRepository(private val attendanceDao: AttendanceDao) {
                 }
 
                 if (attendanceList.isNotEmpty()) {
-                    viewModelScope.launch {
+                    uniConnectScope.launch {
                         attendanceDao.insertAllAttendance(attendanceList)
                     }
                 }
