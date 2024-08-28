f6aa14f93331bbaa798ad28ff5408e74bdb57ab9
mikesplore <mikepremium8@gmail.com>
2024-08-28 17:15:27 +0300

    Shifting to Joins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 20
-        versionName = "1.2.8"
+        versionName = "1.2.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
