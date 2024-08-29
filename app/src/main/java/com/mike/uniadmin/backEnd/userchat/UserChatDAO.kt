f760593f3c4e8bac06afd09d53c62d56adf5ddc6
mikesplore <mikepremium8@gmail.com>
2024-08-29 21:56:33 +0300

    refactoring chats retrieval

--- app/src/main/java/com/mike/uniadmin/backEnd/userchat/UserChatDAO.kt
+++ app/src/main/java/com/mike/uniadmin/backEnd/userchat/UserChatDAO.kt
@@ -1,14 +1,14 @@
 package com.mike.uniadmin.backEnd.userchat
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.mike.uniadmin.UniAdminPreferences
 
 @Dao
 interface UserChatDAO {
-    @Query("SELECT * FROM userChats WHERE path = :path")
-    suspend fun getUserChats(path: String): List<UserChatEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertUserChats(userChats: List<UserChatEntity>)
@@ -18,4 +18,49 @@ interface UserChatDAO {
 
     @Query("UPDATE userChats SET deliveryStatus = :newStatus WHERE id = :messageId")
     suspend fun updateMessageStatus(messageId: String, newStatus: DeliveryStatus)
+
+    @Query(
+        """
+    SELECT uc.*, 
+           sender.id AS sender_id, 
+           sender.firstName AS sender_firstName, 
+           sender.lastName AS sender_lastName, 
+           sender.email AS sender_email, 
+           sender.phoneNumber AS sender_phoneNumber, 
+           sender.profileImageLink AS sender_profileImageLink, 
+           sender.userType AS sender_userType,
+           senderState.online AS sender_state,
+           receiver.id AS receiver_id, 
+           receiver.firstName AS receiver_firstName, 
+           receiver.lastName AS receiver_lastName, 
+           receiver.email AS receiver_email, 
+           receiver.phoneNumber AS receiver_phoneNumber, 
+           receiver.profileImageLink AS receiver_profileImageLink, 
+           receiver.userType AS receiver_userType,
+           receiverState.online AS receiver_state,
+           unreadCounts.unreadCount
+    FROM userChats uc
+    INNER JOIN admins AS sender ON uc.senderID = sender.id
+    LEFT JOIN admins AS receiver ON uc.recipientID = receiver.id
+    LEFT JOIN userState AS senderState ON sender.id = senderState.userID
+    LEFT JOIN userState AS receiverState ON receiver.id = receiverState.userID
+    INNER JOIN (
+        SELECT senderID, MAX(timeStamp) AS latestTimestamp
+        FROM userChats
+        GROUP BY senderID
+    ) latestChats ON uc.senderID = latestChats.senderID AND uc.timeStamp = latestChats.latestTimestamp
+    LEFT JOIN (
+        SELECT senderID, COUNT(*) AS unreadCount
+        FROM userChats
+        WHERE deliveryStatus != 'READ' AND senderID != :currentUserId 
+        GROUP BY senderID
+    ) unreadCounts ON uc.senderID = unreadCounts.senderID
+    ORDER BY uc.timeStamp DESC
+    """
+    )
+    fun getLatestUserChats(currentUserId: String = UniAdminPreferences.userID.value): LiveData<List<UserChatsWithDetails>>
+
+
+
+
 }
