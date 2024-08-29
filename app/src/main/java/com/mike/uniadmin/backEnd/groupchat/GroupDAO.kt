b45cc4a7b5c52a242e13049ec8cb6f7951978dfc
mikesplore <mikepremium8@gmail.com>
2024-08-30 00:32:54 +0300

    refactoring chats retrieval

--- app/src/main/java/com/mike/uniadmin/backEnd/groupchat/GroupDAO.kt
+++ app/src/main/java/com/mike/uniadmin/backEnd/groupchat/GroupDAO.kt
@@ -1,5 +1,6 @@
 package com.mike.uniadmin.backEnd.groupchat
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -7,14 +8,21 @@ import androidx.room.Query
 
 @Dao
 interface GroupChatDao {
-    @Query("SELECT * FROM groupChats WHERE id LIKE '%' || :path || '%'")
-    suspend fun getChats(path: String): List<GroupChatEntity>
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertChats(chats: List<GroupChatEntity>)
 
-    @Query("DELETE FROM groupChats WHERE id = :chatId")
+    @Query("DELETE FROM groupChats WHERE chatId = :chatId")
     suspend fun deleteChat(chatId: String)
+
+    @Query(
+        "SELECT" +
+                "    gc.*," +
+                "    a.firstName AS senderName," +
+                "    a.profileImageLink AS senderProfileImageLink\n" +
+                "FROM groupChats gc\n" +
+                "INNER JOIN admins a ON gc.senderID = a.id;"
+    )
+     fun getChatsWithDetails(): LiveData<List<GroupChatEntityWithDetails>>
 }
 
 @Dao
