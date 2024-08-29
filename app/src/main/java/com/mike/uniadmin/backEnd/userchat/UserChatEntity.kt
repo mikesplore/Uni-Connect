0a618cac38fc96a3922cb09db6fd1fcb25dce024
mikesplore <mikepremium8@gmail.com>
2024-08-29 21:56:29 +0300

    refactoring chats retrieval

--- app/src/main/java/com/mike/uniadmin/backEnd/userchat/UserChatEntity.kt
+++ app/src/main/java/com/mike/uniadmin/backEnd/userchat/UserChatEntity.kt
@@ -1,7 +1,9 @@
 package com.mike.uniadmin.backEnd.userchat
 
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.mike.uniadmin.backEnd.users.UserEntity
 
 @Entity(tableName = "userChats")
 data class UserChatEntity(
@@ -18,8 +20,22 @@ data class UserChatEntity(
     constructor(): this("", "", "", "", "", "", "", DeliveryStatus.SENT)
 }
 
+
+
+data class UserChatsWithDetails(
+    @Embedded val userChat: UserChatEntity,
+    @Embedded(prefix = "sender_") val sender: UserEntity, // Maps sender details
+    @Embedded(prefix = "receiver_") val receiver: UserEntity, // Maps receiver details
+    val senderState: String?, // Sender's state from userState table
+    val receiverState: String?, // Receiver's state from userState table
+    val unreadCount: Int? = 0 // Optional, in case there are no unread counts
+)
+
+
+
+
 enum class DeliveryStatus{
-        SENT,
-        READ,
+    SENT,
+    READ,
 
 }
