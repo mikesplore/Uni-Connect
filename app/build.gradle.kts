6b60e963659b2a3b22949e14a19f2cabeed5fa97
mikesplore <mikepremium8@gmail.com>
2024-08-30 18:00:47 +0300

    refactoring chats retrieval

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 20
-        versionName = "1.2.9"
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
