474d4cc8b0bccf281e078ce5d64e5a232e4a6dbd
mikesplore <mikepremium8@gmail.com>
2024-08-30 17:58:39 +0300

    refactoring chats retrieval

--- app/src/main/java/com/mike/uniadmin/backEnd/moduleContent/moduleAnnouncements/ModuleAnnouncementDao.kt
+++ app/src/main/java/com/mike/uniadmin/backEnd/moduleContent/moduleAnnouncements/ModuleAnnouncementDao.kt
@@ -1,5 +1,6 @@
 package com.mike.uniadmin.backEnd.moduleContent.moduleAnnouncements
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -13,12 +14,15 @@ interface ModuleAnnouncementDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertModuleAnnouncements(moduleAnnouncements: List<ModuleAnnouncement>)
 
+    @Query("DELETE FROM moduleAnnouncements WHERE moduleID = :moduleID")
+    suspend fun clearAnnouncementsForModule(moduleID: String)
+
     @Query(
         "SELECT moduleAnnouncements.*, admins.firstName AS authorName, admins.profileImageLink AS profileImageLink " +
                 "FROM moduleAnnouncements INNER JOIN admins ON moduleAnnouncements.authorID = admins.id " +
                 "WHERE moduleAnnouncements.moduleID = :moduleID"
     )
-    suspend fun getModuleAnnouncements(moduleID: String): List<ModuleAnnouncementsWithAuthor>
+     fun getModuleAnnouncements(moduleID: String): LiveData <List<ModuleAnnouncementsWithAuthor>>
 
     @Query("DELETE FROM moduleAnnouncements WHERE moduleID = :moduleID")
     suspend fun clearAnnouncements(moduleID: String)
