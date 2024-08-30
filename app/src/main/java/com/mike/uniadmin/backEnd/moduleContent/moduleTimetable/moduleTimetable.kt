336e6d050c534b028461959b62d211e80d88fa06
mikesplore <mikepremium8@gmail.com>
2024-08-30 18:00:26 +0300

    refactoring chats retrieval

---
+++ app/src/main/java/com/mike/uniadmin/backEnd/moduleContent/moduleTimetable/moduleTimetable.kt
@@ -0,0 +1,19 @@
+package com.mike.uniadmin.backEnd.moduleContent.moduleTimetable
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "moduleTimetable")
+data class ModuleTimetable(
+   @PrimaryKey val timetableID: String,
+    val day: String = "",
+    val moduleID: String = "",
+    val startTime: String = "",
+    val endTime: String = "",
+    val venue: String = "",
+    val lecturer: String = "",
+    val moduleName: String = ""
+){
+    constructor(): this("", "", "", "", "", "", "","")
+}
+
