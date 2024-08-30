9f8ece609ddd4323747bca41dd13810be3056a9f
mikesplore <mikepremium8@gmail.com>
2024-08-30 18:01:02 +0300

    refactoring chats retrieval

--- app/src/main/java/com/mike/uniadmin/ui/theme/Color.kt
+++ app/src/main/java/com/mike/uniadmin/ui/theme/Color.kt
@@ -11,19 +11,19 @@ import androidx.compose.ui.graphics.Color
 
 
 //light theme
-val LightBlue = Color(0xFFDCEEFB)
+val LightBlue = Color(0xffAEE2FF)
 val PaleBlue = Color(0xFF86B6F6)
 val Cyan = Color(0xFF2980B9)
-val DarkGray = Color(0xFF212121)
+val DarkGray = Color(0xFF0D151E)
 val SkyBlue = Color(0xFFBBDEFB)
 val White = Color(0xFF1B4F72)
 
 
 //dark theme
 val DarkNavy = Color(0xFF0D151E)
-val DeepBlue = Color(0xFF14344B)
-val BrightBlue = Color(0xFFA1C6FA)
+val DeepBlue = Color(0xFF27374D)
+val BrightBlue = Color(0xFF6096B4)
 val LightGray = Color(0xFFF2F2F2)
 val Turquoise = Color(0xFF3C5B6F)
-val Black = Color(0xFF0A192F)
+val Black = Color(0xFFA7E6FF)
 
