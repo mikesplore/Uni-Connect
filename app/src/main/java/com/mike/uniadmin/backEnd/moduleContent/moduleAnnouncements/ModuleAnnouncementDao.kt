d178f9751d23b621367d3ee60d4e80ae3b02786a
mikesplore <mikepremium8@gmail.com>
2024-08-31 22:04:33 +0300

    Fixes the Query for Retrievng last Message and renaming of the users table

--- app/src/main/java/com/mike/uniadmin/backEnd/moduleContent/moduleAnnouncements/ModuleAnnouncementDao.kt
+++ app/src/main/java/com/mike/uniadmin/backEnd/moduleContent/moduleAnnouncements/ModuleAnnouncementDao.kt
@@ -18,8 +18,8 @@ interface ModuleAnnouncementDao {
     suspend fun clearAnnouncementsForModule(moduleID: String)
 
     @Query(
-        "SELECT moduleAnnouncements.*, admins.firstName AS authorName, admins.profileImageLink AS profileImageLink " +
-                "FROM moduleAnnouncements INNER JOIN admins ON moduleAnnouncements.authorID = admins.id " +
+        "SELECT moduleAnnouncements.*, users.firstName AS authorName, users.profileImageLink AS profileImageLink " +
+                "FROM moduleAnnouncements INNER JOIN users ON moduleAnnouncements.authorID = users.id " +
                 "WHERE moduleAnnouncements.moduleID = :moduleID"
     )
      fun getModuleAnnouncements(moduleID: String): LiveData <List<ModuleAnnouncementsWithAuthor>>
