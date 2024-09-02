0c25363a5f9e7dda276f6b35b9f2f49005506a36
mikesplore <mikepremium8@gmail.com>
2024-09-02 15:33:37 +0300

    Big fixing

--- app/src/main/java/com/mike/uniadmin/backEnd/moduleContent/moduleAssignments/ModuleAssignmentsViewModel.kt
+++ app/src/main/java/com/mike/uniadmin/backEnd/moduleContent/moduleAssignments/ModuleAssignmentsViewModel.kt
@@ -3,10 +3,14 @@ package com.mike.uniadmin.backEnd.moduleContent.moduleAssignments
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.mike.uniadmin.backEnd.announcements.uniConnectScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class ModuleAssignmentViewModel(private val repository: ModuleAssignmentRepository) : ViewModel() {
@@ -17,13 +21,25 @@ class ModuleAssignmentViewModel(private val repository: ModuleAssignmentReposito
     private val _isLoading = MutableLiveData(false) // Add isLoading state
     val isLoading: LiveData<Boolean> = _isLoading
 
-    // Fetch assignments for a specific module
+    private var moduleAssignmentsLiveData: LiveData<List<ModuleAssignment>>? = null
+
+    private val moduleAssignmentsObserver = Observer<List<ModuleAssignment>> { moduleAssignments ->
+        _assignments.value = moduleAssignments
+    }
+
     fun getModuleAssignments(moduleID: String) {
-        _isLoading.postValue(true) // Set loading to true before fetching
-        repository.getModuleAssignments(moduleID) { assignments ->
-            _assignments.value = assignments
-            _isLoading.postValue(false) // Set loading to false after fetching
+        uniConnectScope.launch(Dispatchers.Main) {
+            moduleAssignmentsLiveData = withContext(Dispatchers.IO) {
+                repository.getModuleAssignments(moduleID)
+            }
+            moduleAssignmentsLiveData?.observeForever(moduleAssignmentsObserver)
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        moduleAssignmentsLiveData?.removeObserver(moduleAssignmentsObserver)
+        moduleAssignmentsLiveData = null
     }
 
     // Save a new assignment
@@ -41,6 +57,7 @@ class ModuleAssignmentViewModel(private val repository: ModuleAssignmentReposito
         }
     }
 }
+
 
 class ModuleAssignmentViewModelFactory(private val repository: ModuleAssignmentRepository) : ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
