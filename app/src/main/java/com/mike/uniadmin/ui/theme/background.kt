82fa8c689a650f2c234380b2dbdaaa38eb10eb95
mikesplore <mikepremium8@gmail.com>
2024-09-03 18:04:30 +0300

    Big fixing

--- app/src/main/java/com/mike/uniadmin/ui/theme/background.kt
+++ app/src/main/java/com/mike/uniadmin/ui/theme/background.kt
@@ -2,20 +2,12 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Circle
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.mike.uniadmin.helperFunctions.randomColor
-import kotlin.random.Random
 import com.mike.uniadmin.ui.theme.CommonComponents as CC
 
 @Composable
