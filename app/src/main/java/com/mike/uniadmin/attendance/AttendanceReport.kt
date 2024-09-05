e21fc5f4be46d7f229888e26f74e1ed9334e2d50
mikesplore <mikepremium8@gmail.com>
2024-09-05 18:37:15 +0300

    Big fixing

---
+++ app/src/main/java/com/mike/uniadmin/attendance/AttendanceReport.kt
@@ -0,0 +1,8 @@
+package com.mike.uniadmin.attendance
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun  AttendanceReport(context: Context, navController: NavController){}
