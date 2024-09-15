e9dda02268e231a81edf4d896ab7f8da08e28ba8
mikesplore <mikepremium8@gmail.com>
2024-09-15 17:03:31 +0300

    minor improvements

--- app/src/main/java/com/mike/uniadmin/attendance/Attendance.kt
+++ app/src/main/java/com/mike/uniadmin/attendance/Attendance.kt
@@ -2,11 +2,11 @@ package com.mike.uniadmin.attendance
 
 import android.content.Context
 import androidx.compose.runtime.Composable
-import com.mike.uniadmin.UniAdminPreferences
+import com.mike.uniadmin.UniConnectPreferences
 
 @Composable
 fun AttendanceScreen(context: Context) {
-    val userTypes = UniAdminPreferences.userType.value
+    val userTypes = UniConnectPreferences.userType.value
     if (userTypes == "admin") {
         ManageAttendanceScreen(context)
     } else {
