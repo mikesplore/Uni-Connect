032e8474b0e90c93624555d877ec9732013f0cbd
mikesplore <mikepremium8@gmail.com>
2024-09-15 12:39:15 +0300

    minor improvements

--- app/src/main/java/com/mike/uniadmin/uniChat/userChat/userChatComponents/UserChatTopBar.kt
+++ app/src/main/java/com/mike/uniadmin/uniChat/userChat/userChatComponents/UserChatTopBar.kt
@@ -37,7 +37,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
-import com.mike.uniadmin.backEnd.users.UserEntity
+import com.mike.uniadmin.model.users.UserEntity
 import com.mike.uniadmin.helperFunctions.randomColor
 import com.mike.uniadmin.ui.theme.CommonComponents as CC
 
