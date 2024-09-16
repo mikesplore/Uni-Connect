eca9c7fb34abce18ae6fa5f10366e60f474ce330
mikesplore <mikepremium8@gmail.com>
2024-09-16 14:32:56 +0300

    minor improvements

--- app/src/main/java/com/mike/uniadmin/model/courses/CourseEntity.kt
+++ app/src/main/java/com/mike/uniadmin/model/courses/CourseEntity.kt
@@ -1,11 +1,7 @@
 package com.mike.uniadmin.model.courses
 
 import androidx.room.Entity
-import androidx.room.ForeignKey
-import androidx.room.ForeignKey.Companion.CASCADE
-import androidx.room.Index
 import androidx.room.PrimaryKey
-
 
 @Entity(tableName = "courses")
 data class Course(
@@ -17,22 +13,6 @@ data class Course(
 }
 
 
-@Entity(tableName = "enrollments",
-    foreignKeys = [ForeignKey(
-        entity = Course::class,
-        parentColumns = ["courseCode"],
-        childColumns = ["courseCode"],
-        onDelete = CASCADE
-    )],
-    indices = [Index(value = ["courseCode"])] // Add index for courseCode
-)
-data class Enrollment(
-    @PrimaryKey(autoGenerate = true) val enrollmentId: Int = 0,
-    val userId: String, // to identify which user is enrolled
-    val courseCode: String,
-    val enrollmentYear: String // e.g., 2024-2025
-)
-
 @Entity(tableName = "academicYears")
 data class AcademicYear(
     @PrimaryKey val year: String,
@@ -41,15 +21,4 @@ data class AcademicYear(
     constructor() : this("", emptyList())
 }
 
-data class CourseWithEnrollment(
-    val courseCode: String,
-    val courseName: String,
-    val enrollmentYear: String
-) {
-    constructor(course: Course, enrollment: Enrollment) : this(
-        courseCode = course.courseCode,
-        courseName = course.courseName,
-        enrollmentYear = enrollment.enrollmentYear
-    )
-}
 
