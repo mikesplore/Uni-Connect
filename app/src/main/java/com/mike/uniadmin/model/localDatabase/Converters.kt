9b5d019e48392c42bf156673961a6b0f46490d37
mikesplore <mikepremium8@gmail.com>
2024-09-16 03:48:23 +0300

    minor improvements

--- app/src/main/java/com/mike/uniadmin/model/localDatabase/Converters.kt
+++ app/src/main/java/com/mike/uniadmin/model/localDatabase/Converters.kt
@@ -2,6 +2,9 @@ package com.mike.uniadmin.model.localDatabase
 
 
 import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.mike.uniadmin.model.courses.AcademicYear
 
 class Converters {
     @TypeConverter
@@ -12,5 +15,16 @@ class Converters {
     @TypeConverter
     fun toStringList(list: List<String>?): String? {
         return list?.joinToString(",")
+    }
+
+    @TypeConverter
+    fun fromAcademicYearList(value: List<AcademicYear>): String {
+        return Gson().toJson(value)
+    }
+
+    @TypeConverter
+    fun toAcademicYearList(value: String): List<AcademicYear> {
+        val listType = object : TypeToken<List<AcademicYear>>() {}.type
+        return Gson().fromJson(value, listType)
     }
 }
