2629c6f41c9efe42b50c43442780529e5982399a
mikesplore <mikepremium8@gmail.com>
2024-09-16 21:34:31 +0300

    minor improvements

--- app/src/main/java/com/mike/uniadmin/NavGraph.kt
+++ app/src/main/java/com/mike/uniadmin/NavGraph.kt
@@ -33,7 +33,7 @@ import com.mike.uniadmin.uniChat.userChat.UserChatScreen
 @Composable
 fun NavigationGraph(context: Context, mainActivity: MainActivity) {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "settings") {
+    NavHost(navController = navController, startDestination = "splashScreen") {
 
         composable("splashScreen") {
             SplashScreen(navController = navController)
